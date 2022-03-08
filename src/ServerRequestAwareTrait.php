2af86f5ea892bc724554ac52df4c10bd04036415
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-08 23:18:09 +0200

    Update ServerRequestAwareTrait.php

--- src/ServerRequestAwareTrait.php
+++ src/ServerRequestAwareTrait.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ServerRequestInterface;
 
 trait ServerRequestAwareTrait
 {
-    public readonly ?ServerRequestInterface $serverRequest = null;
+    private ?ServerRequestInterface $serverRequest = null;
     public function setServerRequest(ServerRequestInterface $serverRequest): ServerRequestAwareInterface 
     {
       $this->serverRequest = $serverRequest;
